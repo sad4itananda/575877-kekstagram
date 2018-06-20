bac53b79860d85a02098d3386faadbc181b7dead
sad4itananda <4ipej6ij@mail.ru>
2018-06-20 22:08:34 +0300

    first commit

--- js/pictures.js
+++ js/pictures.js
@@ -52,8 +52,8 @@ var generatePhotos = function () {
 
 var showBigPicture = function () {
   document.querySelector('.big-picture').classList.remove('hidden');
-  document.querySelector('.social__comment-count').classList.add('visually-hidden');
-  document.querySelector('.social__loadmore').classList.add('visually-hidden');
+  // document.querySelector('.social__comment-count').classList.add('visually-hidden');
+  // document.querySelector('.social__loadmore').classList.add('visually-hidden');
 };
 
 var renderPhotos = function (array) {
@@ -92,9 +92,89 @@ var getComment = function (element) {
 var init = function () {
   var photos = generatePhotos();
   renderPhotos(photos);
-  showBigPicture();
+  // showBigPicture();
   initBigPictureData(photos[0]);
   getComment(photos[0]);
 };
 
 init();
+// --------------------events-block------------------------------------------------
+var VALUE_MAX = 100;
+var uploadForm = document.querySelector('#upload-file');
+var buttonCancelForm = document.querySelector('#upload-cancel');
+var scalePin = document.querySelector('.scale__pin');
+var scaleValue = document.querySelector('.scale__value').value;
+var effectsList = document.querySelector('.effects__list');
+var imgUploadPreview = document.querySelector('.img-upload__preview');
+var imgUploadScale = document.querySelector('.img-upload__scale');
+var pictures = document.querySelector('section, .pictures');
+var pictureCansel = document.querySelector('#picture-cancel');
+
+var  onUploadFormChange = function () {
+  document.querySelector('.img-upload__overlay')
+  .classList.remove('hidden')
+};
+var onButtonCancelFormClick = function () {
+  document.querySelector('.img-upload__overlay')
+  .classList.add('hidden')
+};
+var  onDocumentKeydown = function(evt) {
+  if (evt.keyCode === 27) {
+    document.querySelector('.img-upload__overlay').classList.add('hidden');
+  }
+};
+var onScalePinMouseup = function () {alert('rabotaet!')};
+
+var onEffectsListClick = function(evt) {
+  switch (evt.target.id) {
+    case 'effect-none':
+    // imgUploadScale.classList.add('hidden');
+    // по тз шкала пропадает , это работает но покая это скрыл.
+    break;
+    case 'effect-chrome':
+    imgUploadPreview.style.cssText = '';
+    imgUploadPreview.style.filter = 'grayscale(' + (scaleValue / VALUE_MAX)  + ')';
+    console.log(imgUploadPreview.style.filter);
+    break;
+    case 'effect-sepia':
+    imgUploadPreview.style.cssText = '';
+    imgUploadPreview.style.filter = 'sepia(' + (scaleValue / VALUE_MAX)  + ')';
+    console.log(imgUploadPreview.style.filter);
+    break;
+    case 'effect-marvin':
+    imgUploadPreview.style.cssText = '';
+    imgUploadPreview.style.filter = 'invert(' + scaleValue  + '%)';
+    console.log(imgUploadPreview.style.filter);
+    break;
+    case 'effect-phobos':
+    imgUploadPreview.style.cssText = '';
+    imgUploadPreview.style.filter = 'blur(' + (scaleValue / VALUE_MAX * 3)  + 'px)';
+    console.log(imgUploadPreview.style.filter);
+    break;
+    case 'effect-heat':
+    imgUploadPreview.style.cssText = '';
+    imgUploadPreview.style.filter = 'brightness(' + (scaleValue / VALUE_MAX * 3)  + ')';
+    console.log(imgUploadPreview.style.filter);
+    break;
+  }
+};
+
+var onDocumentPicturesClick = function(evt) {
+ if (evt.target.className === 'picture__img') {
+  showBigPicture();
+ };
+};
+
+var onPictureCanselClick = function() {
+  document.querySelector('.big-picture')
+  .classList.add('hidden');
+};
+
+pictureCansel.addEventListener('click', onPictureCanselClick)
+uploadForm.addEventListener('change', onUploadFormChange);
+buttonCancelForm.addEventListener('click', onButtonCancelFormClick);
+document.addEventListener('keydown', onDocumentKeydown);
+scalePin.addEventListener('mouseup', onScalePinMouseup);
+effectsList.addEventListener('click', onEffectsListClick);
+document.addEventListener('click', onDocumentPicturesClick);
+
