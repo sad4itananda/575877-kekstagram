f9f9d81258bbf588f7720c4193129e3717becc93
sad4itananda <4ipej6ij@mail.ru>
2018-07-01 14:39:19 +0300

    travis fix 1

--- js/pictures.js
+++ js/pictures.js
@@ -289,13 +289,13 @@ var MAX_HASHTAG_SYMBOLS = 20;
 var resizeMinusSelector = document.querySelector('.resize__control--minus');
 var resizePlusSelector = document.querySelector('.resize__control--plus');
 var resizeValueSelector = document.querySelector('.resize__control--value');
-var valueResize =  MAX_SIZE_PICTURE;
+var valueResize = MAX_SIZE_PICTURE;
 resizeValueSelector.value = MAX_SIZE_PICTURE + ' %';
 
 var onResizeMinusSelectorClick = function () {
   if (valueResize !== MIN_SIZE_PICTURE) {
     valueResize -= MIN_SIZE_PICTURE;
-    resizeValueSelector.value = valueResize +  ' %';
+    resizeValueSelector.value = valueResize + ' %';
     imgUploadPreviewSelector.style.transform = 'scale(' + valueResize / MAX_SIZE_PICTURE + ')';
   }
 };
