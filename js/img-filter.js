48833c88360d74084df4ff928f0630aacad28257
sad4itananda <4ipej6ij@mail.ru>
2018-07-06 18:37:55 +0300

    открыл блок фильтров, добавил обработчик клика по ним

--- js/img-filter.js
+++ js/img-filter.js
@@ -1,3 +1,30 @@
-// 'use strict';
+'use strict';
 
-// var document.querySelector('form.img-filters__form');
+var imgFiltersForm =  document.querySelector('form.img-filters__form');
+
+var onImgFiltersFormClick = function(evt) {
+  switch (evt.target.id) {
+    case 'filter-popular':
+      activeImgFilter(evt);
+      break;
+    case 'filter-new':
+      activeImgFilter(evt);
+      break;
+    case 'filter-discussed':
+      activeImgFilter(evt);
+      break;
+  }
+};
+
+
+ imgFiltersForm.addEventListener('click', onImgFiltersFormClick);
+
+var activeImgFilter = function (evt) {
+  for (var i = 0; i < imgFiltersForm.children.length; i ++) {
+    var list = imgFiltersForm.children[i].classList;
+    if (list.length > 1 ) {
+      list.remove('img-filters__button--active');
+    };
+    evt.target.classList.add('img-filters__button--active');
+  }
+};
